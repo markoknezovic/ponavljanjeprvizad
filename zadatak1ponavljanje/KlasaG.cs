883beeef7888e3908925f37c3068ef8a22439938
markoknezovic <58472152+markoknezovic@users.noreply.github.com>
2021-06-07 09:21:37 +0200

    Add files via upload

---
+++ zadatak1ponavljanje/KlasaG.cs
@@ -0,0 +1,16 @@
+﻿namespace zadatak1ponavljanje
+{
+    public class KlasaG
+    {
+
+        public void Plus10(int a)
+        {
+            a += 10;
+        }
+
+        public void f50(int b)
+        {
+            b += 50;
+        }
+    }
+}
---
+++ zadatak1ponavljanje/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak1ponavljanje
+{
+    
+    public class Program
+
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+
+    
+
+        
+    }
+}
+    
+
